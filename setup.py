728b472a1ec43c11e743c45e42596d198a035679
tota <grabantot@gmail.com>
2019-07-28 11:14:22 +0300

    upd setup.py

--- setup.py
+++ setup.py
@@ -1,16 +1,17 @@
 """
-Deuces: A pure Python poker hand evaluation library
+Deuces-numpy: Numpy version of deuces package
 """
 
 from setuptools import setup
 
 setup(
-    name='deuces',
+    name='deuces-numpy',
     version='0.1',
     description=__doc__,
-    long_description=open('README.md').read(),
-    author='Will Drevo',
-    url='https://github.com/worldveil/deuces',
+    long_description='descr',
+    long_description_content_type='text/plain',
+    author='Anatol Grabowski',
+    url='https://github.com/grabantot/deuces-numpy',
     license='MIT',
     packages=['deuces'],
     classifiers=[
